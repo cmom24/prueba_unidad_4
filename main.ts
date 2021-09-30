25d75ccdaa96d63b58e86ec798320b6c6271974e
cmom24 <91328671+cmom24@users.noreply.github.com>
2021-09-29 22:34:24 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,23 @@
+let Cantidad_Agua = 0
+let Dias_menos_2mm = 0
+let Agua_Diaria = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        Cantidad_Agua = 0
+        Dias_menos_2mm = 0
+        for (let index = 0; index < 1825; index++) {
+            Agua_Diaria = randint(0, 6)
+            Cantidad_Agua += Agua_Diaria
+            if (Agua_Diaria <= 2) {
+                Dias_menos_2mm += 1
+            }
+        }
+        basic.showNumber(Cantidad_Agua)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showNumber(Dias_menos_2mm)
+    }
+})
